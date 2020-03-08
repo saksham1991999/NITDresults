fa8e88acd7874722bc6f5da77489c8850c0c4933
Rahul Dev <r.dev2000@gmail.com>
2020-03-08 16:38:05 +0530

    updated settings for database

--- nitdresults/settings.py
+++ nitdresults/settings.py
@@ -127,9 +127,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
 PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
