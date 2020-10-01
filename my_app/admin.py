168ef08786c9d3e9195f9cd58ee3e63ef4ab8215
Saksham Mittal <saksham1991999@users.noreply.github.com>
2020-10-02 04:58:09 +0530

    admin and urls
    registered the model in admin site and updated the admin site header.
    updated the urls after specifying the app name.

--- my_app/admin.py
+++ my_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from my_app import models
 # Register your models here.
+
+admin.site.site_header = 'NITD Results'
+
+admin.site.register(models.Student)
--- my_app/urls.py
+++ my_app/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'my_app'
+
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('search', views.search, name='search'),
